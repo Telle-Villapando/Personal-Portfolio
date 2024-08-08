ba3cc47c8574911d95412207881f4bee48f9ba36
ChristelleD_V <89006081+Telle-Villapando@users.noreply.github.com>
2024-08-08 17:06:19 +0800

    updated-na-updated
    ....

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let navbar = document.querySelector('.navbar');
 let sections = document.querySelector('section');
 let navLinks = document.querySelector('header nav a');
 
+
 window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
@@ -25,4 +26,5 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 }
+
 
